4a11d260f61706f50058e5c71bd92d154ce0cd1f
Rob Sameh <rob.sameh@gmail.com>
2022-01-25 20:57:31 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("p").click(fucntion() {
+    $(".Alligator").show();
+  });
+});
+  // const animal = $("#Animal").val(); ;
+
+  // if (animal === "Alligator") {
+  //   $('#Alligator').show();
+  //   $('#Alligator').hide():
+  // } else if (animal === "Elephant") {
+  //   $('#Elephant').show();
+  // } else (animal === "Emu") {
+  //   $('#Emu').show();
+  // } 
+
+});
